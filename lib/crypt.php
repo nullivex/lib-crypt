32f0db4efd58cfa86e3f7f9c37d787483b4462d8
Nullivex <contact@nullivex.com>
2013-03-06 22:35:36 -0800

    installer fixes

--- lib/crypt.php
+++ lib/crypt.php
@@ -49,14 +49,14 @@ class Crypt {
 
 	//setup and store keys
 	protected function __construct($key,$iv){
-		self::$key = $key;
-		self::$iv = $iv;
+		$this->key = $key;
+		$this->iv = $iv;
 	}
 
 	public function verify(){
-		self::$verifyKey();
-		self::$verifyIV();
-		self::$verified = true;
+		$this->verifyKey();
+		$this->verifyIV();
+		$this->verified = true;
 		return $this;
 	}
 
@@ -64,10 +64,10 @@ class Crypt {
 	protected function verifyKey(){
 		$key_size = mcrypt_get_key_size(self::$crypt_cipher,self::$crypt_mode);
 		//verify we have a key before starting
-		if(!isset(self::$key) || is_null(self::$key)){
+		if(!isset($this->key) || is_null($this->key)){
 			throw new Exception('No encryption key defined in config');
 		}
-		if(strlen(base64_decode(self::$key)) < $key_size){
+		if(strlen(base64_decode($this->key)) < $key_size){
 			throw new Exception('Encryption key is too shorted, required length: '.$key_size);
 		}
 		return true;
@@ -77,10 +77,10 @@ class Crypt {
 	protected function verifyIV(){
 		$iv_size = mcrypt_get_iv_size(self::$crypt_cipher,self::$crypt_mode);
 		//verify we have a key before starting
-		if(!isset(self::$iv) || is_null(self::$iv)){
+		if(!isset($this->iv) || is_null($this->iv)){
 			throw new Exception('No IV key defined in config');
 		}
-		if(strlen(base64_decode(self::$iv)) < $iv_size){
+		if(strlen(base64_decode($this->iv)) < $iv_size){
 			throw new Exception('Encryption IV is too shorted, required length: '.$iv_size);
 		}
 		return true;
@@ -88,14 +88,14 @@ class Crypt {
 
 	//encrypt string and optionally base64_encode
 	public function encrypt($plain_string,$base64_encode=true){
-		if(!self::$verified) self::$verify();
+		if(!$this->verified) $this->verify();
 		//encrypt and return
 		$enc_string = mcrypt_encrypt(
 			 self::$crypt_cipher
-			,base64_decode(self::$key)
+			,base64_decode($this->key)
 			,$plain_string
 			,self::$crypt_mode
-			,base64_decode(self::$iv)
+			,base64_decode($this->iv)
 		);
 		if($base64_encode) return base64_encode($enc_string);
 		return $enc_string;
@@ -104,15 +104,15 @@ class Crypt {
 	//decrypt string from an optionally base64_encoded source
 	function decrypt($enc_string,$base64_decode=true){
 		if(is_null($enc_string) || empty($enc_string)) return NULL;
-		if(!self::$verified) self::$verify();
+		if(!$this->verified) $this->verify();
 		//decrypt and return
 		if($base64_decode) $enc_string = base64_decode($enc_string);
 		return rtrim(mcrypt_decrypt(
 			 self::$crypt_cipher
-			,base64_decode(self::$key)
+			,base64_decode($this->key)
 			,$enc_string
 			,self::$crypt_mode
-			,base64_decode(self::$iv)
+			,base64_decode($this->iv)
 		),"\0");
 	}
 
