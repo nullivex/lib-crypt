36d10233853c3b615344725474d20112385c256d
Nullivex <contact@nullivex.com>
2013-03-13 17:19:35 -0700

    fixes to array by reference stuff fixed install scripts

--- lib/crypt.php
+++ lib/crypt.php
@@ -128,7 +128,7 @@ class Crypt {
 			,base64_decode($this->iv)
 		);
 		//get size of string from atom
-		$size = mda_shift(unpack('N',substr($plain_string,0,4)));
+		$size = array_shift(unpack('N',substr($plain_string,0,4)));
 		//make sure we have the original string
 		$plain_string = substr($plain_string,4,$size);
 		return $plain_string;
