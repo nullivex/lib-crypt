21c988a95601525131b29420af27b5d48a8b4c3a
Colin Li <cboisworld@gmail.com>
2013-03-23 04:49:08 -0400

    added licensing and namespace to classes

--- lib/crypt.php
+++ lib/crypt.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ *  OpenLSS - Lighter Smarter Simpler
+ *
+ *	This file is part of OpenLSS.
+ *
+ *	OpenLSS is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Lesser General Public License as
+ *	published by the Free Software Foundation, either version 3 of
+ *	the License, or (at your option) any later version.
+ *
+ *	OpenLSS is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the 
+ *	GNU Lesser General Public License along with OpenLSS.
+ *	If not, see <http://www.gnu.org/licenses/>.
+*/
+namespace LSS;
+
 if(!extension_loaded('mcrypt')) throw new Exception('MCrypt extension not present!');
 //---------------------------------------------------------
 //Crypt Library
