1e2e05089daf2e482d2d6e10f6d3e9fbee2b260c
nullivex <contact@nullivex.com>
2013-03-26 01:21:13 -0700

    added autoloading

--- LSS/Crypt.php
+++ LSS/Crypt.php
@@ -17,7 +17,7 @@
  *	You should have received a copy of the 
  *	GNU Lesser General Public License along with OpenLSS.
  *	If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 namespace LSS;
 
 if(!extension_loaded('mcrypt')) throw new Exception('MCrypt extension not present!');
